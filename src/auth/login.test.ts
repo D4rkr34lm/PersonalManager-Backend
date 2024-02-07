d02ace08005559318a1c6176aaac80adebc5b229
D4rkr34lm <darth.m.frohn@gmail.com>
2024-02-07 23:32:37 +0100

    Added unit test for login

---
+++ src/auth/login.test.ts
@@ -0,0 +1,35 @@
+import pg from 'pg'
+import env from 'dotenv'
+
+import handleLogin from "./login"
+
+env.config()
+
+const credentials = {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
+}
+
+const pool = new pg.Pool(credentials)
+
+test('Login: Valid', async () =>{
+    
+    const client = await pool.connect()
+    expect(await handleLogin('test', 'test', client)).toBeDefined()
+    client.release()
+})
+
+test('Login: password invalid', async () =>{
+    const client = await pool.connect()
+    await expect(handleLogin('test', 'tes', client)).rejects.toMatch('Invalid credentials')
+    client.release()
+})
+
+test('Login: username invalid', async () =>{
+    const client = await pool.connect()
+    await expect(handleLogin('tes', 'test', client)).rejects.toMatch('Invalid credentials')
+    client.release()
+})
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import pg from "pg"
 import cors from "cors"
 import jwt, { JwtPayload } from "jsonwebtoken"
 
-import handleLogin from './login/login'
+import handleLogin from './auth/login'
 import getData from './data/data'
 
 
@@ -12,74 +12,74 @@ env.config()
 
 const credentials = {
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    port: parseInt(process.env.DATABASE_PORT),
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD
-};
+}
 
-const pool = new pg.Pool(credentials);
+const pool = new pg.Pool(credentials)
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.options('*', cors())
 
 app.post("/login", async (req, res) => {
     if(req.body.password && req.body.username){
-        const client = await pool.connect();
+        const client = await pool.connect()
 
         try{
-            const token = await handleLogin(req.body.password, req.body.username, client);
+            const token = await handleLogin(req.body.password, req.body.username, client)
 
-            console.log(`[INF] Auth for ${req.body.username} succeded`);
-            res.status(200).send(token);
+            console.log(`[INF] Auth for ${req.body.username} succeded`)
+            res.status(200).send(token)
         }
         catch (err){
-            console.log(`[INF] Auth for ${req.body.username} failed`);
-            res.status(401).end();
+            console.log(`[INF] Auth for ${req.body.username} failed`)
+            res.status(401).end()
         }
 
-        client.release();
+        client.release()
     }
     else{
-        console.log("[WRN] Auth request with incorect payload configuration");
-        res.status(400);
-        res.end();
+        console.log("[WRN] Auth request with incorect payload configuration")
+        res.status(400)
+        res.end()
     }
 });
 
 app.get("/data", (req, res) => {
     if(!req.headers.authorization){
-        console.log("[WRN] Request without auth recived");
-        res.status(403).end();
+        console.log("[WRN] Request without auth recived")
+        res.status(403).end()
     }
         
 
-    const auth = req.headers.authorization.split(' ');
+    const auth = req.headers.authorization.split(' ')
     if(auth[0] == "Bearer"){
         jwt.verify(auth[1], process.env.HASHING_SECRET, async (err, token : JwtPayload) =>{
             if(err){
-                console.log("[INF] Request with invalid token recived");
-                res.status(403).end(); 
+                console.log("[INF] Request with invalid token recived")
+                res.status(403).end()
             }
             else{
                 const username = token.username;
-                console.log(`[INF] Recived data request for user ${username}`);
-                const client = await pool.connect();
-                const containers = await getData(username, client);
+                console.log(`[INF] Recived data request for user ${username}`)
+                const client = await pool.connect()
+                const containers = await getData(username, client)
                 const body = {containers: containers}
 
-                res.status(200).json(body);
+                res.status(200).json(body)
             }
         });
     }
     else{
-        console.log("[WRN] Request with faulty auth recived");
-        res.status(403).end();
+        console.log("[WRN] Request with faulty auth recived")
+        res.status(403).end()
     }
 });
 
-app.listen(9000, () => console.log("[INF] Server started, listening to port 9000"))
+app.listen(9001, () => console.log("[INF] Server started, listening to port 9000"))
